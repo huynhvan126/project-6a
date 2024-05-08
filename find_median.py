c4002cbde1e7d8a2ec6f18dd5f8a5e50178fcc44
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-05-08 16:23:57 -0700

    Add files via upload

---
+++ find_median.py
@@ -0,0 +1,15 @@
+# Author: Van Huynh
+# GitHub username: huynhvan126
+# Date: 05/08/2024
+# Description:
+def find_median(numbers):
+    if not numbers:
+        return None
+    sorted_numbers = sorted (numbers)
+    length = len(numbers)
+    middle = length // 2
+    if length % 2 == 0:
+        median = (sorted_numbers[middle - 1] + sorted_numbers[middle])
+    else:
+        median = (sorted_numbers[middle])
+    return median
